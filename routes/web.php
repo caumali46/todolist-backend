9be755bcb64365a0e67f76f610fdfae857d08b00
catherineumali <caumali46@gmail.com>
2022-09-20 03:06:08 +0800

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization");
+Route::options("/{any:.*}", [function (){ 
+   return response(["status" => "success"]); 
+  }
+ ]
+);
 
 $router->get('/', function () use ($router) {
     try {
@@ -27,4 +34,7 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api/v1'], function() use ($router) {
     $router->get('/tasks', 'Task\GetAllTaskAction@getAll');
+    $router->post('/tasks', 'Task\AddTaskAction@addNewTask');
+    $router->put('/tasks/{id', 'Task\CompleteTaskAction@completeTask');
+    $router->delete('/tasks/{id}', 'Task\DeleteTaskAction@delete');
 });
